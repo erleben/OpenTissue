827e8fa72b71efeae1f8b497608758ca2b2f4467
Kenny Erleben <kenny@di.ku.dk>
2020-07-24 13:41:28 +0200

    Update install_boost_bindings.bat

--- third_party/include/install_boost_bindings.bat
+++ third_party/include/install_boost_bindings.bat
@@ -1,2 +1,2 @@
-svn export http://svn.boost.org/svn/boost/sandbox/numeric_bindings-v1/boost/numeric/bindings boost_bindings/boost/numeric/bindings
-svn export http://svn.boost.org/svn/boost/sandbox/numeric_bindings-v1/libs/numeric/bindings boost_bindings/libs/numeric/bindings
+svn export https://svn.boost.org/svn/boost/sandbox/numeric_bindings-v1/boost/numeric/bindings boost_bindings/boost/numeric/bindings
+svn export https://svn.boost.org/svn/boost/sandbox/numeric_bindings-v1/libs/numeric/bindings boost_bindings/libs/numeric/bindings
